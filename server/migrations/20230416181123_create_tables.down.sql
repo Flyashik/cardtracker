38c2c05b568f59d4ad5e5fd93b36cc5cfb19db30
Flyagin ID <flyagin00@mail.ru>
2023-05-30 01:19:56 +0300

    уведомления

--- server/migrations/20230416181123_create_tables.down.sql
+++ server/migrations/20230416181123_create_tables.down.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS user_phone;
+
 DROP TABLE IF EXISTS sim_cards;
 
 DROP TABLE IF EXISTS sd_cards;
@@ -5,7 +7,5 @@ DROP TABLE IF EXISTS sd_cards;
 DROP TABLE IF EXISTS phones;
 
 DROP TABLE IF EXISTS users;
-
-DROP TABLE IF EXISTS user_phone;
 
 DROP TABLE IF EXISTS notifications;
