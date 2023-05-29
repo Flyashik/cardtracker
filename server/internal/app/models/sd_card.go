1ee2080cc0dc37792cdcdf689085ff562f0b320b
Flyagin ID <flyagin00@mail.ru>
2023-05-29 15:30:54 +0300

    поправки в конституцию

--- server/internal/app/models/sd_card.go
+++ server/internal/app/models/sd_card.go
@@ -2,7 +2,7 @@ package models
 
 type SdInfo struct {
 	Id               int    `json:"sd_card_id"`
-	PhoneId          int    `json:"phone_id"`
+	PhoneId          *int   `json:"phone_id"`
 	SdManufacturerId string `json:"sd_manufacturer_id"`
 	SerialNo         string `json:"serial_no"`
 	TotalSpace       int    `json:"total_space"`
