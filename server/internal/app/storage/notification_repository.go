2aefff4ae14e78700e7c04b031b4ea85d5e2c35f
Andrew Nikolaev <Andrew.Vaimon@yandex.ru>
2023-05-30 01:53:33 +0300

    Update notification_repository.go

--- server/internal/app/storage/notification_repository.go
+++ server/internal/app/storage/notification_repository.go
@@ -9,7 +9,7 @@ type NotificationRepository struct {
 }
 
 func (r *NotificationRepository) Create(n *models.Notification) (*models.Notification, error) {
-	err := r.storage.db.QueryRow(`INSERT INTO notifications (model_number,source,sender,body,timestamp)
+	err := r.storage.db.QueryRow(`INSERT INTO notifications (model_number,notification_source,sender,body,timestamp)
 										VALUES ($1, $2, $3, $4, $5) RETURNING notification_id`,
 		n.ModelTag, n.Source, n.Sender, n.Body, n.Timestamp).Scan(&n.Id)
 	if err != nil {
